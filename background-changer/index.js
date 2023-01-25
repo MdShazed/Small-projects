810fc21c033d38a0ce696c79b9412acb44db6ee3
Md-Shazed <123171557+Md-shazed@users.noreply.github.com>
2023-01-25 21:48:59 +0600

    Add files via upload

---
+++ background-changer/index.js
@@ -0,0 +1,9 @@
+console.log('hello world');
+
+function change_color(){
+    let color_input = document.getElementById('color_input').value;
+    let btn = document.getElementById('btn');
+
+    document.body.style.backgroundColor = color_input ; 
+
+}
